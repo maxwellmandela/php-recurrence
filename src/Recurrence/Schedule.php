718c1f190d0b55a0685c47d0a3f96b393d6b83a6
Maxwell Mandela <mxmandela@gmail.com>
2019-07-07 17:36:35 +0300

    LICENSE info

--- src/Recurrence/Schedule.php
+++ src/Recurrence/Schedule.php
@@ -1,5 +1,20 @@
 <?php
 
+/*
+ * Copyright 2019 Maxwell Mandela
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Based on simshaun/recurr
+ * Copyright (c) 2015 Shaun Simmons
+ * https://github.com/simshaun/recurr/blob/master/LICENSE
+ *
+ * Based on nesbot/carbon
+ * Copyright (C) Brian Nesbitt
+ * https://github.com/briannesbitt/Carbon/blob/master/LICENSE
+ */
+
 namespace Reccurence;
 
 use Carbon\Carbon;
